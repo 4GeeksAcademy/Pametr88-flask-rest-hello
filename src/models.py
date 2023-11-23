67f8da6c4fdd538c993fd17ab2abc8346061eac2
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-11-23 21:47:19 +0000

    agregue modelos

--- src/models.py
+++ src/models.py
@@ -7,13 +7,137 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.String(120), unique=True, nullable=False)
+    mass = db.Column(db.String(120), unique=True, nullable=False)
+    hair_color = db.Column(db.String(120), unique=True, nullable=False)
+    skin_color = db.Column(db.String(120), unique=True, nullable=False)
+    eye_color = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='character', lazy=True)
+    
+    def __repr__(self):
+        return '<Character %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            # do not serialize the password, its a security breach
+        }
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.String(120), unique=True, nullable=False)
+    rotation_period = db.Column(db.String(120), unique=True, nullable=False)
+    orbital_period = db.Column(db.String(120), unique=True, nullable=False)
+    gravity = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='planets', lazy=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            # do not serialize the password, its a security breach
+        }
+    
+class Films(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    created = db.Column(db.String(120), unique=True, nullable=False)
+    edited = db.Column(db.String(120), unique=True, nullable=False)
+    producer = db.Column(db.String(120), unique=True, nullable=False)
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    director = db.Column(db.String(120), unique=True, nullable=False)
+    release_date = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='films', lazy=True)
+
+    def __repr__(self):
+        return '<Films %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created": self.created,
+            "edited": self.edited,
+            "producer": self.producer,
+            "title": self.title,
+            "director": self.director,
+            "release_date": self.relase_date,
+           # do not serialize the password, its a security breach
+        }
+class Species(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    classification = db.Column(db.String(120), unique=True, nullable=False)
+    designation = db.Column(db.String(120), unique=True, nullable=False)
+    average_height = db.Column(db.String(120), unique=True, nullable=False)
+    average_lifespan = db.Column(db.String(120), unique=True, nullable=False)
+    hair_colors = db.Column(db.String(120), unique=True, nullable=False)
+    skin_colors = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='species', lazy=True)
+
+    def __repr__(self):
+        return '<Species %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "classification": self.classification,
+            "designation": self.designation,
+            "average_height": self.average_height,
+            "average_lifespan": self.average_lifespan,
+            "hair_colors": self.hair_colors,
+            "skin_colors": self.skin_colors,
+            # do not serialize the password, its a security breach
+        }
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    films_id = db.Column(db.Integer, db.ForeignKey('films.id'))
+    species_id = db.Column(db.Integer, db.ForeignKey('species.id'))
+    
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+
             # do not serialize the password, its a security breach
         }
